93141dc4f7ff473d681ec50e974dd6b0f0d9ee06
EC2 Default User <ec2-user@ip-172-31-82-229.ec2.internal>
2019-10-12 11:26:41 +0000

    created file structure and added navbar to base.html

--- lukesproject/settings.py
+++ lukesproject/settings.py
@@ -38,6 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 'django_forms_bootstrap',
+    # 'authentication',
+    # 'cart',
+    # 'checkout',
+    # 'product',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +125,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+
+MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
